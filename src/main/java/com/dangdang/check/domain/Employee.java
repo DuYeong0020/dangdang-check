03cfb855ce4da0885e69344d8bc45e4c3a9e7604
duyeong0020 <duyeong0020@gmail.com>
2025-02-18 00:15:30 +0900

    feat: 직원 엔티티 추가

---
+++ src/main/java/com/dangdang/check/domain/Employee.java
@@ -0,0 +1,27 @@
+package com.dangdang.check.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class Employee extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(unique = true, nullable = false)
+    private String loginId;
+    @Column(nullable = false)
+    private String password;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String mobilePhone; // 휴대전화
+    @Enumerated(EnumType.STRING)
+    private Role role;
+}
