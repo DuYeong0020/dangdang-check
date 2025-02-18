bd6f2c306d817c1fcbcf04f86b034d8088088c37
duyeong0020 <duyeong0020@gmail.com>
2025-02-19 00:11:31 +0900

    feat: 미용 기록을 위한 GroomingRecord, GroomingRecordImage 엔티티 추가 - 반려견 미용 내역을 저장하는 GroomingRecord 엔티티 추가 - 어떤 반려견이 언제 미용을 받았는지 저장 - 미용 스타일 및 특이사항 기록 가능 - 미용 사진 저장할 수 있도록 GroomingRecordImage 엔티티 추가

---
+++ src/main/java/com/dangdang/check/domain/GroomingRecord.java
@@ -0,0 +1,25 @@
+package com.dangdang.check.domain;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class GroomingRecord extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Lob
+    private String groomingDetails; // 미용 스타일 및 특이사항
+    private LocalDateTime groomedAt; // 미용한 날짜
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "pet_id", nullable = false)
+    private Pet pet; // 미용받은 반려동물
+}
