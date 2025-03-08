d669b007e36934efd61e5adec952c28fcc996ead
duyeong0020 <duyeong0020@gmail.com>
2025-03-08 11:46:13 +0900

    feat: 로그인된 사용자 정보를 주입하는 ArgumentResolver 추가

---
+++ src/main/java/com/dangdang/check/common/argumentresolver/LoginEmployeeArgumentResolver.java
@@ -0,0 +1,29 @@
+package com.dangdang.check.common.argumentresolver;
+
+import org.springframework.core.MethodParameter;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.support.WebDataBinderFactory;
+import org.springframework.web.context.request.NativeWebRequest;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.method.support.ModelAndViewContainer;
+
+public class LoginEmployeeArgumentResolver implements HandlerMethodArgumentResolver {
+    @Override
+    public boolean supportsParameter(MethodParameter parameter) {
+        boolean hasLoginAnnotation = parameter.hasParameterAnnotation(Login.class);
+        boolean hasStringType = String.class.isAssignableFrom(parameter.getParameterType());
+        return hasLoginAnnotation && hasStringType;
+    }
+
+    @Override
+    public Object resolveArgument(MethodParameter parameter, ModelAndViewContainer mavContainer, NativeWebRequest webRequest, WebDataBinderFactory binderFactory) throws Exception {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+        if (authentication == null || authentication.getPrincipal() == null) {
+            throw new IllegalStateException("SecurityContext에 인증 정보가 없습니다.");
+        }
+
+        return authentication.getName();
+    }
+}
