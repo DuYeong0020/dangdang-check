f7d8b32aec3d2c6177da5d369f88110b298c4ba9
duyeong0020 <duyeong0020@gmail.com>
2025-03-09 15:49:12 +0900

    feat: 활성화된 가게 조회 메서드 추가

--- src/main/java/com/dangdang/check/infrastrucure/store/StoreJpaRepository.java
+++ src/main/java/com/dangdang/check/infrastrucure/store/StoreJpaRepository.java
@@ -3,5 +3,8 @@ package com.dangdang.check.infrastrucure.store;
 import com.dangdang.check.domain.store.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface StoreJpaRepository extends JpaRepository<Store, Long> {
+    Optional<Store> findByIdAndIsDeletedFalse(Long id);
 }
