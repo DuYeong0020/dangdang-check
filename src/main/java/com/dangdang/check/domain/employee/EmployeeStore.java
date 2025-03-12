83aedc6c1cc1bc28c638f75392f27cd4437f7ec5
duyeong0020 <duyeong0020@gmail.com>
2025-03-12 20:03:54 +0900

    feat: Store 직원 초기화 메서드 추가

--- src/main/java/com/dangdang/check/domain/employee/EmployeeStore.java
+++ src/main/java/com/dangdang/check/domain/employee/EmployeeStore.java
@@ -3,4 +3,6 @@ package com.dangdang.check.domain.employee;
 public interface EmployeeStore {
 
     Employee storeEmployee(Employee employee);
+
+    void resetEmployeesOfStore(Long storeId);
 }
--- src/main/java/com/dangdang/check/infrastrucure/employee/EmployeeJpaStore.java
+++ src/main/java/com/dangdang/check/infrastrucure/employee/EmployeeJpaStore.java
@@ -2,6 +2,10 @@ package com.dangdang.check.infrastrucure.employee;
 
 import com.dangdang.check.domain.employee.Employee;
 import com.dangdang.check.domain.employee.EmployeeStore;
+import com.dangdang.check.domain.employee.QEmployee;
+import com.dangdang.check.domain.employee.Role;
+import com.dangdang.check.domain.store.Store;
+import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
@@ -10,9 +14,21 @@ import org.springframework.stereotype.Component;
 public class EmployeeJpaStore implements EmployeeStore {
 
     private final EmployeeJpaRepository employeeJpaRepository;
+    private final JPAQueryFactory jpaQueryFactory;
 
     @Override
     public Employee storeEmployee(Employee employee) {
         return employeeJpaRepository.save(employee);
+    }
+
+    @Override
+    public void resetEmployeesOfStore(Long storeId) {
+        QEmployee employee = QEmployee.employee;
+        jpaQueryFactory.update(employee)
+                .set(employee.store, (Store) null)
+                .set(employee.role, Role.DEFAULT)
+                .where(employee.store.id.eq(storeId)
+                        .and(employee.isDeleted.eq(false)))
+                .execute();
     }
 }
