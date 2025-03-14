36dd8557b9a5e7b55cf0e40cf70f39f393156989
duyeong0020 <duyeong0020@gmail.com>
2025-03-14 18:23:42 +0900

    feat: 고객 조회 기능을 위한 CustomerJpaReader 및 CustomerReader 인터페이스 추가

--- src/main/java/com/dangdang/check/infrastrucure/customer/CustomerJpaRepository.java
+++ src/main/java/com/dangdang/check/infrastrucure/customer/CustomerJpaRepository.java
@@ -3,5 +3,9 @@ package com.dangdang.check.infrastrucure.customer;
 import com.dangdang.check.domain.customer.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CustomerJpaRepository extends JpaRepository<Customer, Long> {
+
+    Optional<Customer> findByIdAndIsDeletedFalse(Long id);
 }
