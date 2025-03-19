b92cfeafef6eb720cd5a4851bb0808d5232aca8c
duyeong0020 <duyeong0020@gmail.com>
2025-03-19 15:50:55 +0900

    feat: 고객 수정 엔드포인트 및 요청/응답 DTO 추가

--- src/main/java/com/dangdang/check/interfaces/customer/CustomerApiController.java
+++ src/main/java/com/dangdang/check/interfaces/customer/CustomerApiController.java
@@ -7,9 +7,7 @@ import com.dangdang.check.domain.customer.CustomerInfo;
 import com.dangdang.check.domain.customer.CustomerService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -23,6 +21,15 @@ public class CustomerApiController {
         CustomerCommand.RegisterCustomerWithPetsRequest command = request.toCommand(loginId);
         CustomerInfo customerInfo = customerService.registerCustomerWithPets(command);
         CustomerDto.RegisterCustomerWithPetsResponse response = new CustomerDto.RegisterCustomerWithPetsResponse(customerInfo);
+        return CommonResponse.success(response);
+    }
+
+    @PatchMapping("/api/customers/{customerId}")
+    public CommonResponse<CustomerDto.ModifyCustomerResponse> modifyCustomer(@Login String loginId, @PathVariable Long customerId,
+                                                       @RequestBody @Valid CustomerDto.ModifyCustomerRequest request) {
+        CustomerCommand.ModifyCustomerRequest command = request.toCommand(loginId, customerId);
+        CustomerInfo customerInfo = customerService.modifyCustomer(command);
+        CustomerDto.ModifyCustomerResponse response = new CustomerDto.ModifyCustomerResponse(customerInfo);
         return CommonResponse.success(response);
     }
 }
