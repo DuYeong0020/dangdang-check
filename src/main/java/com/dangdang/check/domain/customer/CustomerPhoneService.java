9e2ce3249dd2b3bfa99b41c7dc185bb15726ab84
duyeong0020 <duyeong0020@gmail.com>
2025-03-20 13:12:32 +0900

    feat: add modifyCustomerPhone method to CustomerPhoneService

--- src/main/java/com/dangdang/check/domain/customer/CustomerPhoneService.java
+++ src/main/java/com/dangdang/check/domain/customer/CustomerPhoneService.java
@@ -3,4 +3,6 @@ package com.dangdang.check.domain.customer;
 public interface CustomerPhoneService {
 
     CustomerPhoneInfo registerCustomerPhone(CustomerPhoneCommand.RegisterPhoneRequest request);
+
+    CustomerPhoneInfo modifyCustomerPhone(CustomerPhoneCommand.ModifyCustomerPhoneRequest request);
 }
